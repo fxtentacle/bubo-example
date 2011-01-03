1c267127baa33b10e0069dc99c54c792ceb7cb5a
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2011-01-03 20:32:08 +0100

    Switch from bubo.key = url to bubo.key = image.id

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,5 +1,6 @@
 module SearchHelper
   def preview_url(url)
+    url = Image.find(url.to_i).url
     if url =~ /.*?@spratpix-fs\/(.*?)\/full\/(.*)/
       url = 'https://pict-01.spratshop.com/spratpix-FS/get/c/'+$1+'/wm250/'+$2
     end
