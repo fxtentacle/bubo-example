971e5e4892ff4a14c9a2b32e4f8c72692f953b52
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2011-01-03 19:48:49 +0100

    Result view should work now :)

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,8 @@
 module SearchHelper
+  def preview_url(url)
+    if url =~ /.*?@spratpix-fs\/(.*?)\/full\/(.*)/
+      url = 'https://pict-01.spratshop.com/spratpix-FS/get/c/'+$1+'/wm250/'+$2
+    end
+    url
+  end
 end
