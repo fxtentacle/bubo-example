23821bc694b955f00f3383fcde74e3d6b674920c
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2011-01-03 19:54:46 +0100

    Ass stylesheets + remove public /images route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :images
+  # map.resources :images
   map.root :controller => "search"
   map.connect '/search/result', :controller => "search", :action => "result"
   map.connect '/search/result2', :controller => "search", :action => "result2"
