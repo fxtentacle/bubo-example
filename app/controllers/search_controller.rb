b8258c7b8d83125da13492ab2d0121f2c0ea4b8b
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2011-01-04 16:59:30 +0100

    Update search controller and /result and /result2 to enable searching by upload as well as by url.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,12 +4,19 @@ class SearchController < ApplicationController
   end
   
   def result
-    @rnd = ActiveSupport::SecureRandom.base64(6)
-    File.open("#{RAILS_ROOT}/tmp/query", "wb") { |f| f.write(params[:upload].read) }
+    if params[:url]
+      @query_img_url = params[:url]
+    else
+      @rnd = ActiveSupport::SecureRandom.base64(6)
+      File.open("#{RAILS_ROOT}/tmp/query", "wb") { |f| f.write(params[:upload].read) }
+      @query_img_url = (url_for :controller => 'search', :action => 'query') + '?rnd='+@rnd 
+    end
+    
+    @result2_url = url_for :controller => 'search', :action => 'result2', :q => @query_img_url
   end
   
   def result2
-    @query_img_url = (url_for :controller => 'search', :action => 'query') + '?rnd='+params[:rnd]
+    @query_img_url = params[:q]
     @retrieval = make_bubo.retrieve(@query_img_url)
   end
   
